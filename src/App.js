d492739b95dc113fee083c3736b0a0d8f4410012
dhathri <dhathrivupparapalli@gmail.com>
2021-10-04 21:06:30 +0530

    added form component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Form from "./components/Form";
+
+function App() {
+    return (
+        <div className="container-fluid">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-12 col-md-10 col-lg-7">
+                        <Form />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
