eafb976b14b8171f85fba84f7cc212ee3a27ae65
Bably <bablyd15@gmail.com>
2021-10-05 22:06:45 +0530

    form component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+
 import Form from "./components/Form";
 
 function App() {
     return (
-        <div className="container-fluid">
+        <div className="container-fluid text-center py-5">
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-12 col-md-10 col-lg-7">
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,22 @@ import React from "react";
 function Form() {
     return (
         <div>
-            <h1> Form Data </h1>
+            <h1> to-do app </h1>
+            <div class="mb-3">
+                <label
+                    for="exampleFormControlInput1"
+                    class="form-label"
+                ></label>
+                <input
+                    type="text"
+                    class="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="task name"
+                />
+            </div>
+            <div>
+                <button>submit</button>
+            </div>
         </div>
     );
 }
