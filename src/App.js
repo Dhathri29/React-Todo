c8b235ff2ca94656ceba77bf0ac31616ad908ed0
The Bug <66660068+arkajyotiadhikary@users.noreply.github.com>
2021-10-06 13:56:06 +0530

    todo input style changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-12 col-md-10 col-lg-7">
+                        <h1 className="h1"> to-do app </h1>
+                    </div>
+                </div>
+                <div className="row justify-content-center mt-5">
+                    <div className="col-12 col-md-10 col-lg-7">
                         <Form />
                     </div>
                 </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -13,25 +13,22 @@ const Form = () => {
         });
     };
     return (
-        <div>
-            <h1> to-do app </h1>
-            <div class="mb-3">
-                <label
-                    for="exampleFormControlInput1"
-                    class="form-label"
-                ></label>
-                <input
-                    type="text"
-                    class="form-control"
-                    id="exampleFormControlInput1"
-                    placeholder="task name"
-                    name="text"
-                    onChange={formChange}
-                    value={text}
-                />
+        <div className="d-flex justify-content-center">
+            <div>
+                <label for="exampleFormControlInput1" className="form-label">
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="exampleFormControlInput1"
+                        placeholder="task name"
+                        name="text"
+                        onChange={formChange}
+                        value={text}
+                    />
+                </label>
             </div>
             <div>
-                <button>submit</button>
+                <button className="btn btn-primary mx-2">submit</button>
             </div>
         </div>
     );
